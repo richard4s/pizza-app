91ae9d74004bab799267d42f1fbecaff136c5265
richard4s <richardoluwo50@gmail.com>
2019-09-07 06:11:35 +0100

    Can fetch all pizzas

--- src/components/card-section.js
+++ src/components/card-section.js
@@ -6,19 +6,47 @@ const axios = require('axios');
 
 export default class CardSection extends React.Component {
 
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-    }
+    // }
+
+    state = {
+        isLoading: true,
+        pizza: [],
+        error: null
+      }
 
     grabPizza() {
-        axios({
-            method: 'get',
-            url: 'https://myconve.live',
-            responseType: 'json'
-          }).then(function (response) {
-                console.log(response.data.success);
+        axios
+        // // The API we're requesting data from
+        // .get("http://127.0.0.1:8000/api/pizzas")
+        // // Once we get a response, we'll map the API endpoints to our props
+        // .then(response =>
+        //   response.data.pizza.map(pizza => ({
+        //     productName: `${user.name.first} ${user.name.last}`,
+        //     username: `${user.login.username}`,
+        //     email: `${user.email}`,
+        //     image: `${user.picture.thumbnail}`
+        //   }))
+        // )
+        // // Let's make sure to change the loading state to display the data
+        // .then(users => {
+        //   this.setState({
+        //     pizza,
+        //     isLoading: false
+        //   });
+        // })
+        // // We can still use the `.catch()` method since axios is promise-based
+        // .catch(error => this.setState({ error, isLoading: false }));
+        .get("http://127.0.0.1:8000/api/pizzas")
+        .then(response => {
+            this.setState({
+              pizza: response.data.pizza,
+              isLoading: false
             });
+          })
+        .catch(error => this.setState({ error, isLoading: false }));
     }
 
     componentDidMount() {
@@ -26,55 +54,39 @@ export default class CardSection extends React.Component {
     }
 
     render() {
+
+        const { isLoading, pizza, error } = this.state;
+
         return(
             <div className="card-sec">
                 <Container>
                     <Row className="justify-content-md-center">
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body className="align-center">
-                                    <Card.Title>Pepperoni</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><FiPlusCircle /> Add to Cart</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
 
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body className="align-center">
-                                    <Card.Title>Chicken</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><FiPlusCircle /> Add to Cart</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                    {error ? <p>{error.message}</p> : null}
 
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body className="align-center">
-                                    <Card.Title>Beef</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><FiPlusCircle /> Add to Cart</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                    {!isLoading ? (
+                        pizza.map(pizzas => {
+                        const { productName, productPrice, productImage, productDescription } = pizzas;
+                        return ( 
+                            <Col style={{marginBottom: '40px'}}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={productImage} />
+                                    <Card.Body className="align-center">
+                                        <Card.Title> {productName} </Card.Title>
+                                        <Card.Text> {productDescription} </Card.Text>
+                                        <Card.Text> € {productPrice} </Card.Text>
+                                        <Button variant="primary"><FiPlusCircle /> Add to Cart</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        );
+                        })
+                    ) : (
+                        <p>Loading Pizzas <span role="img" aria-label="pizza">🍕</span>...</p>
+                    )}
+
                     </Row>
-                </Container>
-
-                  
+                </Container>                  
             </div>
         )
     }
